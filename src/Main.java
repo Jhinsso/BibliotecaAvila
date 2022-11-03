39ab74209c91476eaa029addd1be287a1346d46d
Jorge Barrio <115726013+Jhinsso@users.noreply.github.com>
2022-11-03 17:46:45 +0100

    F1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner= new Scanner;
         System.out.println("Hello world!");
+        Genre genre= new Genre();
+        System.out.println("Introduce un genero:");
+        genre.setId(scanner.nextInt());
+
+        System.out.println("Introduce el NOMBRE de un genero");
+        genre.setName(scanner.next());
     }
 }
