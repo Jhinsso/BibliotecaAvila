b517e24add849ab7adbb081daef984b40c431806
Jorge Barrio <115726013+Jhinsso@users.noreply.github.com>
2022-11-03 18:18:18 +0100

    F2

--- src/Libros.java
+++ src/Libros.java
@@ -1,2 +1,33 @@
 public class Libros {
+    private Integer isbn;
+    private String title;
+    private String autor;
+    private String genre;
+
+    public Integer getIsbn(){
+        return isbn;
+    }
+    public void  setIsbn(Integer isbn){
+        this.isbn = isbn;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getAutor(){
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public String getBirthday () {
+        return birthday;
+    }
+    public void setBirthday(String birthday){
+        this.birthday = birthday;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
-
 public class Main {
+
     public static void main(String[] args) {
-        Scanner scanner= new Scanner;
-        System.out.println("Hello world!");
+
+        Scanner scanner= new Scanner(System.in);
+
         Genre genre= new Genre();
         System.out.println("Introduce un genero:");
         genre.setId(scanner.nextInt());
 
         System.out.println("Introduce el NOMBRE de un genero");
         genre.setName(scanner.next());
+
+        System.out.println("Introduce una descripcion del genero");
+        genre.setDescription(scanner.next());
     }
 }
