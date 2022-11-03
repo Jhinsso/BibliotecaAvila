d31bc6e8efe2222a93aab86929bc09f028ca6fc8
Jorge Barrio <115726013+Jhinsso@users.noreply.github.com>
2022-11-03 17:55:22 +0100

    F2

--- src/Autor.java
+++ src/Autor.java
@@ -1,3 +1,30 @@
 public class Autor {
+    private Integer id;
+    private String name ;
+    private String subname;
+    private String birthday;
 
+    public Integer getId(){
+        return id;
+    }
+    public void  setId(Integer id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getSubname(){
+        return subname;
+    }
+    public void setSubname(String description) {
+        this.subname = subname;
+    }
+    public String getBirthday () {
+        return birthday;
+        }
 }
