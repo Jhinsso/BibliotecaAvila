f7125397f5c9a2d8ef7d64f456c4e07f63af35cf
Jorge Barrio <115726013+Jhinsso@users.noreply.github.com>
2022-11-10 18:42:28 +0100

    F5

--- src/common/Prestados.java
+++ src/common/Prestados.java
@@ -1,4 +1,27 @@
 package common;
 
 public class Prestados {
+
+    private Integer id;
+    private String datstart;
+    private String datend;
+
+    public Integer getId(){
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getDatstart(){
+        {return datstart;}
+    }
+    public void setDatstart(String datestart)
+    {this.datstart=datstart;}
+    public String getDatend(){
+        return datend;
+    }
+    public void setDatend(String datend){
+        this.datend=datend;
+    }
 }
